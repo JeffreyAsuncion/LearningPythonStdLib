8d8e682e2b60b3eeec41c288eb3e7691a5c8fb4d
JeffreyAsuncion <jeffrey.l.asuncion@gmail.com>
2022-02-25 15:17:18 -0500

    Chapters

---
+++ Chapter04/04_07.py
@@ -0,0 +1,22 @@
+# HTTP Package
+
+string_url = 'https://www.googleapis.com/books/v1/volumes?q=isbn:1101904224'
+
+import urllib.request 
+import json
+import textwrap
+
+
+with urllib.request.urlopen(string_url) as f:
+    text = f.read()
+    decodedtext=text.decode('utf-8')
+    print(textwrap.fill(decodedtext, width=50))
+print()
+
+
+obj = json.loads(decodedtext)
+
+print(obj['kind'])
+
+print(obj['items'][0]['searchInfo']['textSnippet'])
+
